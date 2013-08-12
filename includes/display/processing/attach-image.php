be84c7cbad8ecd09128917222bc676626e1e098f
Kevin Stover <kstover@gmail.com>
2013-08-12 10:20:43 -0400

    Fixed a bug that caused a UL and an empty file upload field to be output when editing posts using Front-End Editor.

--- includes/display/processing/attach-image.php
+++ includes/display/processing/attach-image.php
@@ -176,22 +176,25 @@ function ninja_forms_post_edit_file_attachment_filter( $data, $field_id ){
 		}
 		if( isset( $field_row['data']['featured_image'] ) AND $field_row['data']['featured_image'] == 1 ){
 			$post_thumbnail_id = get_post_thumbnail_id( $post->ID );
-			$attach_field = get_post_meta( $post_thumbnail_id, 'ninja_forms_field_id', true );
-			if( $attach_field == '' ){
-				$file_key = ninja_forms_field_uploads_create_key( array() );
-				$upload_id = '';				
-				$filename = basename ( get_attached_file( $post_thumbnail_id ) );
-				$filepath = str_replace( $filename, '', get_attached_file( $post_thumbnail_id ) );
-				$data['default_value'][$file_key] = array(
-					'user_file_name' => $filename,
-					'file_name' => $filename,
-					'file_path' => $filepath,
-					'file_url' => wp_get_attachment_url( $post_thumbnail_id ),
-					'complete' => 1,
-					'upload_id' => $upload_id
-				);
+			if ( $post_thumbnail_id != '' ) {
+				$attach_field = get_post_meta( $post_thumbnail_id, 'ninja_forms_field_id', true );
+				if( $attach_field == '' ){
+					$file_key = ninja_forms_field_uploads_create_key( array() );
+					$upload_id = '';				
+					$filename = basename ( get_attached_file( $post_thumbnail_id ) );
+					$filepath = str_replace( $filename, '', get_attached_file( $post_thumbnail_id ) );
+					$data['default_value'][$file_key] = array(
+						'user_file_name' => $filename,
+						'file_name' => $filename,
+						'file_path' => $filepath,
+						'file_url' => wp_get_attachment_url( $post_thumbnail_id ),
+						'complete' => 1,
+						'upload_id' => $upload_id
+					);
+				}				
 			}
-		}	
+
+		}
 
 		if( isset( $data['default_value'] ) AND is_array( $data['default_value'] ) ){
 			uasort($data['default_value'], 'ninja_forms_compare_file_name');
