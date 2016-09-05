c51ed0f18d49b618d8b1c562a49d64974061fdb1
Iain <iain@polevaultweb.com>
2016-09-05 08:35:46 +0100

    Don't translate internal string

--- includes/fields/upload.php
+++ includes/fields/upload.php
@@ -126,7 +126,7 @@ class NF_FU_Fields_Upload extends NF_Abstracts_Field {
 				'file_name'       => $file_name,
 				'file_path'       => $target_file,
 				'file_url'        => $file_url,
-				'upload_location' => __( 'server', 'ninja-forms-uploads' ),
+				'upload_location' => 'server',
 				'complete'        => 0,
 			);
 
