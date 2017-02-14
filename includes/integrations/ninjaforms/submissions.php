604c78d7d578a88d54d9bd923fb23a062668aa96
Iain Poulson <iain@polevaultweb.com>
2017-02-14 21:47:55 +0000

    Refactor and use method to get string representation of files for export CSV value. (#199)

--- includes/integrations/ninjaforms/submissions.php
+++ includes/integrations/ninjaforms/submissions.php
@@ -9,10 +9,10 @@ class NF_FU_Integrations_NinjaForms_Submissions {
 	 */
 	public function __construct() {
 		add_filter( 'ninja_forms_custom_columns', array( $this, 'submission_table_row_value' ), 10, 2 );
-
+		// Export value
+		add_filter( 'ninja_forms_subs_export_field_value_' . NF_FU_File_Uploads::TYPE, array( $this, 'submission_export_value' ) );
 		// TODO Edit sub value
 		// TODO Front end editor edit sub
-		// TODO Export submissions
 		// TODO PDF?
 	}
 
@@ -30,6 +30,29 @@ class NF_FU_Integrations_NinjaForms_Submissions {
 			return $value;
 		}
 
+		return $this->get_field_value_string( $value, true );
+	}
+
+	/**
+	 * Format the export CSV value to a comma separated list of file URLs.
+	 *
+	 * @param mixed $field_value
+	 *
+	 * @return string
+	 */
+	public function submission_export_value( $field_value ) {
+		return $this->get_field_value_string( $field_value );
+	}
+
+	/**
+	 * Helper to get string of submitted file URLs.
+	 *
+	 * @param mixed $value
+	 * @param bool  $link Wrap URL in HTML anchor.
+	 *
+	 * @return array|string
+	 */
+	protected function get_field_value_string( $value, $link = false ) {
 		if ( ! is_array( $value ) ) {
 			return $value;
 		}
@@ -44,10 +67,17 @@ class NF_FU_Integrations_NinjaForms_Submissions {
 				continue;
 			}
 
-			$file_url    = NF_File_Uploads()->controllers->uploads->get_file_url( $file_url, $upload->data );
-			$new_value[] = sprintf( '<a href="%s" target="_blank">%s</a>', $file_url, $upload->file_name );
+			$file_url = NF_File_Uploads()->controllers->uploads->get_file_url( $file_url, $upload->data );
+			if ( $link ) {
+				$new_value[] = sprintf( '<a href="%s" target="_blank">%s</a>', $file_url, $upload->file_name );
+				continue;
+			}
+
+			$new_value[] = $file_url;
 		}
 
-		return implode( '<br>', $new_value );
+		$glue = $link ? '<br>' : ',';
+
+		return implode( $glue, $new_value );
 	}
 }
