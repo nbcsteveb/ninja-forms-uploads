c4b02d7002752ddba328f5e70eaf2f15de07b40f
Iain <iain@polevaultweb.com>
2017-03-24 15:42:49 +0000

    Add {field:xxx:raw} mergetag for just comma separated list of URLs with no HTML.

--- includes/integrations/ninjaforms/mergetags.php
+++ includes/integrations/ninjaforms/mergetags.php
@@ -9,6 +9,7 @@ class NF_FU_Integrations_NinjaForms_MergeTags {
 	 */
 	public function __construct() {
 		add_filter( 'ninja_forms_merge_tag_value_' . NF_FU_File_Uploads::TYPE, array( $this, 'merge_tag_value' ), 10, 2 );
+		add_filter( 'ninja_forms_submission_actions', array( $this, 'add_raw_mergetag' ) , 10, 2 );
 	}
 
 	/**
@@ -57,5 +58,40 @@ class NF_FU_Integrations_NinjaForms_MergeTags {
 		}
 
 		return implode( '<br>', $return );
+	}
+
+	/**
+	 * Add raw mergetag which is a comma separated list of URLs. Eg. {field:xxx:raw}
+	 *
+	 * @param array $actions
+	 * @param array $data
+	 *
+	 * @return array
+	 */
+	public function add_raw_mergetag( $actions, $data ) {
+		$all_merge_tags = Ninja_Forms()->merge_tags;
+
+		foreach ( $data['fields'] as $field ) {
+			if ( NF_FU_File_Uploads::TYPE !== $field['settings']['type'] ) {
+				continue;
+			}
+
+			$existing_values = $all_merge_tags['fields']->get_merge_tags();
+			$existing_value  = $existing_values[ 'field_' . $field['settings']['key'] ]['field_value'];
+
+			$dom = new DomDocument();
+			$dom->loadHTML( $existing_value );
+			$output = array();
+			foreach ( $dom->getElementsByTagName( 'a' ) as $item ) {
+				$output[] = $item->getAttribute( 'href' );
+			}
+			$value = implode( ',', $output );
+			$all_merge_tags['fields']->add( 'field_' . $field['settings']['key'] . '_raw', $field['settings']['key'], "{field:{$field['settings']['key']}:raw}", $value );
+		}
+
+		// Save merge tags
+		Ninja_Forms()->merge_tags = $all_merge_tags;
+
+		return $actions;
 	}
 }
