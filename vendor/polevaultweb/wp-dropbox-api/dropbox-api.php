2aea9bc038aaf949a0391e7b0f94ea2d11b46502
Iain <iain@polevaultweb.com>
2017-06-24 14:57:05 +0100

    New Dropbox API method to convert OAuth1 token to OAuth2.

--- vendor/polevaultweb/wp-dropbox-api/dropbox-api.php
+++ vendor/polevaultweb/wp-dropbox-api/dropbox-api.php
@@ -6,7 +6,7 @@ class PVW_Dropbox_API {
 	const CONTENT_URL_BASE = 'https://content.dropboxapi.com/2/';
 	protected $access_token;
 
-	public function __construct( $access_token ) {
+	public function __construct( $access_token = null ) {
 		$this->access_token = $access_token;
 	}
 
@@ -52,13 +52,36 @@ class PVW_Dropbox_API {
 		return $this->post( $endpoint, $args, false );
 	}
 
-	protected function post( $endpoint, $args, $content_api = true ) {
-		$url      = ( $content_api ? self::CONTENT_URL_BASE : self::API_URL_BASE ) . $endpoint;
-		$defaults = array(
+	public function access_token_upgrade( $app_key, $app_secret, $token, $token_secret ) {
+		$endpoint = 'auth/token/from_oauth1';
+
+		$args = array(
 			'headers' => array(
-				'Authorization' => 'Bearer ' . $this->access_token,
+				'Content-Type'  => 'application/json',
+				'Authorization' => 'Basic ' . base64_encode( $app_key . ':' . $app_secret ),
 			),
+			'body'    => json_encode( array( 'oauth1_token' => $token, 'oauth1_token_secret' => $token_secret ) ),
 		);
+
+		$new_token = $this->post( $endpoint, $args, false, false );
+
+		if ( $new_token && isset( $new_token->oauth2_token ) ) {
+			return $new_token->oauth2_token;
+		}
+
+		return false;
+	}
+
+	protected function post( $endpoint, $args, $content_api = true, $auth = true ) {
+		$url      = ( $content_api ? self::CONTENT_URL_BASE : self::API_URL_BASE ) . $endpoint;
+		$defaults = array();
+		if ( $auth ) {
+			$defaults = array(
+				'headers' => array(
+					'Authorization' => 'Bearer ' . $this->access_token,
+				),
+			);
+		}
 
 		$args   = array_merge_recursive( $args, $defaults );
 		$result = wp_remote_post( $url, $args );
